e170ff1c29255a5a69a4f0278d031d4b0d87273a
younesHassan <105860222+younesHassan@users.noreply.github.com>
2022-09-19 23:05:29 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,18 @@
 #include "main.h"
+
+
+
 /**
+
  * _atoi - convert a string to an integer.
+
  * @s: char type string
+
  * Return: integer converted
+
  */
+
+
 
 int _atoi(char *s)
 
@@ -16,15 +25,29 @@ int _atoi(char *s)
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 
 	{
+
 		if (s[firstNum] == '-')
+
 		{
+
 			sign *= -1;
+
+
+
 		}
+
 	}
+
 	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+
 	{
+
 		resp *= 10;
+
 		resp += (s[i] - 48);
+
 	}
+
 	return (sign * resp);
+
 }
